5389ea1dd124e3d294b90f12bfffc8a3705502bf
artelidiot <127449373+artelidiot@users.noreply.github.com>
2023-10-30 12:12:56 -0400

    Use Paper's Adventure integration

--- src/main/java/me/artel/feather/messaging/Messenger.java
+++ src/main/java/me/artel/feather/messaging/Messenger.java
@@ -1,59 +1,49 @@
 package me.artel.feather.messaging;
 
 import de.themoep.minedown.adventure.MineDown;
-import lombok.Getter;
-import me.artel.feather.integration.Wrapper;
-import net.kyori.adventure.audience.Audience;
-import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-/**
- * @apiNote You must initialize your plugin with {@link Wrapper} to use this class
- */
 public class Messenger {
-    @Getter
-    private static final BukkitAudiences audiences = BukkitAudiences.create(Wrapper.getPlugin());
 
     /**
-     * Send a MineDown parsed string to any desired Bukkit Audience
+     * Parse a String through MineDown into an Adventure {@link Component}
      *
-     * @param audience - Any audience present in Bukkit
-     * @param message - The message to be sent
+     * @param input - The String to be parsed
+     * @return - The MineDown parsed Adventure Component
      */
-    public static void send(Audience audience, String message) {
-        Component processed = MineDown.parse(message);
-
-        audience.sendMessage(processed);
+    public static Component parseMineDown(String input) {
+        return MineDown.parse(input);
     }
 
     /**
-     * Send a MineDown parsed string to a Player
+     * Send a MineDown parsed String to a Player
      *
      * @param player - The recipient (Player) of the message
      * @param message - The message to be sent
      */
     public static void send(Player player, String message) {
-        send(audiences.player(player), message);
+        player.sendMessage(parseMineDown(message));
     }
 
     /**
-     * Send a MineDown parsed string to a CommandSender
+     * Send a MineDown parsed String to a CommandSender
      *
      * @param commandSender - The recipient (CommandSender) of the message
      * @param message - The message to be sent
      */
     public static void send(CommandSender commandSender, String message) {
-        send(audiences.sender(commandSender), message);
+        commandSender.sendMessage(parseMineDown(message));
     }
 
     /**
-     * Send a MineDown parsed string to Console
+     * Send a MineDown parsed String to Console
      *
      * @param message - The message to be sent
      */
     public static void sendConsole(String message) {
-        send(audiences.console(), message);
+        Bukkit.getConsoleSender().sendMessage(parseMineDown(message));
     }
 }
--- src/main/java/me/artel/feather/metrics/bStats.java
+++ src/main/java/me/artel/feather/metrics/bStats.java
@@ -6,6 +6,7 @@ import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
+ * TODO: Replace this with a unified Builder class which supports more metrics systems.
  * @apiNote You must initialize your plugin with {@link Wrapper} to use this class
  */
 public class bStats {
